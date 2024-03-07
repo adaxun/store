f628ec0f5aa5a9ad14df752f10016da9abb4b397
zhaowenxun <zhaowenxun2000@163.com>
2024-03-07 10:07:01 +0800

    init

---
+++ store-server/src/main/java/com/wenxun/StoreApplication.java
@@ -0,0 +1,16 @@
+package com.wenxun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wenxun
+ */
+@SpringBootApplication
+public class StoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
+
+}
